ad024906132986cc3c96f51ad19413dfa649a2dd
Yu-Yueh Liu <yu-yueh@hotmail.com>
2018-02-05 01:31:09 -0500

    Clean MainActivity and rewrote how to get fragments, Add App Icon v1, add dummy icons for tabs

--- ExpenseTracker/app/src/main/java/com/example/yueh/ExpenseTracker/MainActivity.java
+++ ExpenseTracker/app/src/main/java/com/example/yueh/ExpenseTracker/MainActivity.java
@@ -13,202 +13,99 @@ import android.support.v7.widget.Toolbar;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements ListAdapter, PaymentList.onPaymentAddedListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     TabLayout MyTabs;
     ViewPager MyPage;
+    Toolbar MyToolbar;
+
     private PayLogs fragment1;
     private Fragment2 fragment2;
     private Fragment3 fragment3;
 
-    //View
-    ListView _paymentListView = null;
-
-    //Data model
-    PaymentList _paymentModel = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        MyToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(MyToolbar);
+
+        //return button!
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Hook ViewPager up to pagerAdapter
         MyPage = (ViewPager) findViewById(R.id.MyPage);
-
+        setupViewPager(MyPage);
         // TEMPORARY SOLUTION TO AVOID SAVING STATES
         MyPage.setOffscreenPageLimit(3);
-        PagerAdapter pagerAdapter = new MyViewPageAdapter(getSupportFragmentManager());
-        MyPage.setAdapter(pagerAdapter);
 
         //Hook up TabLayout
         MyTabs = (TabLayout) findViewById(R.id.MyTabs);
         MyTabs.setupWithViewPager(MyPage);
+        setupTabIcons();
     }
 
-    //
+    private void setupTabIcons() {
+        int[] tabIcons = {
+                R.drawable.ic_tab_favourite,
+                R.drawable.ic_tab_call,
+                R.drawable.ic_tab_contacts
+        };
+
+        MyTabs.getTabAt(0).setIcon(tabIcons[0]);
+        MyTabs.getTabAt(1).setIcon(tabIcons[1]);
+        MyTabs.getTabAt(2).setIcon(tabIcons[2]);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        MyViewPageAdapter adapter = new MyViewPageAdapter(getSupportFragmentManager());
+        adapter.addFrag(new PayLogs(), "ONE");
+        adapter.addFrag(new Fragment2(), "TWO");
+        adapter.addFrag(new Fragment3(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
     // ViewPagerAdapter
-    //
     public class MyViewPageAdapter extends FragmentPagerAdapter{
-        public MyViewPageAdapter(FragmentManager fm){
-            super(fm);
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public MyViewPageAdapter(FragmentManager manager) {
+            super(manager);
         }
 
-        // Array of fragments to cache?
-        private final SparseArray<Fragment> registeredFragments = new SparseArray<>();
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
+//        // Array of fragments to cache?
+//        private final SparseArray<Fragment> registeredFragments = new SparseArray<>();
 
         @Override
         public Fragment getItem(int position) {
-            switch(position) {
-                case 0:
-                    return new PayLogs();
-                case 1:
-                    return new Fragment2();
-                case 2:
-                    return new Fragment3();
-                default:
-                    return null;
-            }
+            return mFragmentList.get(position);
         }
 
         @Override
-        public CharSequence getPageTitle(int position){
-            switch(position) {
-                case 0:
-                    return getString(R.string.Fragment1_title);
-                case 1:
-                    return getString(R.string.Fragment2_title);
-                case 2:
-                    return getString(R.string.Fragment3_title);
-                default:
-                    return null;
-            }
+        public int getCount() {
+            return mFragmentList.size();
         }
 
-        // Safely save a reference to the created Fragment,
-        // no matter where it came from (either getItem() or
-        // FragmentManger). Simply save the returned Fragment from
-        // super.instantiateItem() into an appropriate reference depending
-        // on the ViewPager position.
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+
         @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            Fragment createdFragment = (Fragment) super.instantiateItem(container, position);
-            // save the appropriate reference depending on position
-            switch (position) {
-                case 0:
-                    fragment1 = (PayLogs) createdFragment;
-                    break;
-                case 1:
-                    fragment2 = (Fragment2) createdFragment;
-                    break;
-                case 2:
-                    fragment3 = (Fragment3) createdFragment;
-                    break;
-            }
-            return createdFragment;
-        }
+        public CharSequence getPageTitle(int position) {
 
-        // Possibly useful methods for caching fragments??
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            registeredFragments.remove(position);
-            super.destroyItem(container, position, object);
-        }
-
-        public Fragment getRegisteredFragment(int position) {
-            return registeredFragments.get(position);
-        }
-
-        public SparseArray<Fragment> getRegisteredFragments() {
-            return registeredFragments;
+            // return null to display only the icon
+            // returnm mFragmentTitleList.get(position) for pageTitle
+            return null;
         }
     }
-
-
-    //
-    // Android.widget.listAdapter methods
-    //
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int getCount() {
-        return _paymentModel.getCount();
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return _paymentModel.getPayment(i);
-    }
-
-    @Override
-    public int getItemViewType(int i) {
-        return 0;
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return 1;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        TextView messageView = new TextView(this);
-        messageView.setText(_paymentModel.getPayment(i).getName());
-//        messageView.setText(Double.toString(_paymentModel.getPayment(i).getAmount()));
-//        messageView.setText(_paymentModel.getPayment(i).getRestaurant());
-
-        return messageView;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-
-    @Override
-    public boolean areAllItemsEnabled() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled(int i) {
-        return false;
-    }
-
-    @Override
-    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
-    }
-
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-    }
-
-    // Implementation of onPaymentAddedListener methods
-    @Override
-    public void onPaymentAdded(Payment[] payments) {
-        _paymentListView.invalidateViews();
-    }
-
 }
 
